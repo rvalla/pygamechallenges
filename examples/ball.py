51e15a62d2a6c81ad767d31302f36610e99d0f71
Rodrigo Valla <rodrigovalla@protonmail.ch>
2025-09-11 22:30:56 -0300

    Working with Ball()...

--- examples/ball.py
+++ examples/ball.py
@@ -11,18 +11,20 @@ class Ball():
 
     #Definimos una función para actualizar la pelota:
     def update(self):
-        self.speed[0] += self.acceleration[0]
-        self.speed[1] += self.acceleration[1]
-        self.position[0] += self.speed[0]
-        self.position[1] += self.speed[1]
+        self.speed.x += self.acceleration.x
+        self.speed.y += self.acceleration.y
+        self.position.x += self.speed.x
+        self.position.y += self.speed.y
 
     #Definimos una función para acelerar la pelota:
     def accelerate(self, acceleration):
-        self.acceleration = acceleration
+        self.acceleration.x = acceleration[0]
+        self.acceleration.y = acceleration[1]
 
     #Definimos una función para establecer una velocidad:
     def set_speed(self, speed):
-        self.speed = speed
+        self.speed.x = speed[0]
+        self.speed.y = speed[1]
 
     def set_speed_x(self, speed_x):
-        self.speed[0] = speed_x
+        self.speed.x = speed_x
